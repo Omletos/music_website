68ad6d5c64e9f2321fa20055396b30abdd12a63d
FriedSnakes <126204563+FriedSnakes@users.noreply.github.com>
2023-05-31 09:31:33 +1200

    Create index.php
    initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Music Website</title>
+    <link href="music_website/style.css" rel="stylesheet" type="text/css"/>
+  </head>
+  <body>
+    <header>
+      <nav class="navbar">
+        <div class="logo">
+          <h1>Music</h1>
+        </div>
+        <div class="nav-links">
+          <a href="music website/alex.php">Alex</a>
+          <a href="music website/fahim.php">Fahim</a>
+          <a href="music website/noah.php">Noah</a>
+          <a href="music website/riley.php">Riley</a>
+        </div>
+      </nav>
+    </header>
+    <!-- Rest of your content -->
+  </body>
+</html>
